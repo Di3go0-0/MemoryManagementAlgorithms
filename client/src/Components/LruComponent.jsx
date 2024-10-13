8880991d78c19f6db1ff9221fb759d1b15a7dcbf
Di3go0-0 <d.rincon@utp.edu.co>
2024-10-13 12:51:44 -0500

    Others

--- client/src/Components/LruComponent.jsx
+++ client/src/Components/LruComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./css/style.css";
 import LRU from "../Algorithms/LRU.js";
+
 import { useGlobalContext } from "../Context/Context";
 
 function LruComponent() {
