35c50f39632f9bbc4df3fa1999e38e861524f527
Di3go0-0 <d.rincon@utp.edu.co>
2024-10-14 10:04:27 -0500

    Create Navigation from * route to /

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import { ContextProvider } from "./Context/Context";
 
@@ -10,6 +10,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </BrowserRouter>
       </ContextProvider>
